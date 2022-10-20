d1beb28ce52da4c27c1fb2f809b7cf796519e655
zachmanac <zachstephenson94@gmail.com>
2022-10-20 13:58:25 -0700

    eslinted head function

--- head.js
+++ head.js
@@ -1,5 +1,5 @@
 const head = function(array) {
-    return array[0];
+  return array[0];
 };
 
 
@@ -16,4 +16,4 @@ const assertEqual = function(actual, expected) {
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined)
+assertEqual(head([]), undefined);
