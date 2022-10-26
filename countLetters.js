92c1b7957a20d576724e383c9109342204142d74
zachmanac <zachstephenson94@gmail.com>
2022-10-26 16:14:35 -0700

    added countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    return console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  
+  let count = {};
+
+  for (let character of string) {
+    if(character === " ") {
+      continue;
+    }
+    if (count[character]) {
+      count[character] += 1;
+    } else {
+      count[character] = 1;
+    }
+  }
+  
+  return count;
+};
+
+countLetters("Hello World");
+console.log(countLetters("Hello World"));
