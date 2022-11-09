c96b29e25efc9c4e43dde38a1042e14236742d1a
zachmanac <zachstephenson94@gmail.com>
2022-11-09 14:08:42 -0800

    refactored head, assertEqual, added test folder

--- head.js
+++ head.js
@@ -2,18 +2,4 @@ const head = function(array) {
   return array[0];
 };
 
-
-
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-  if (actual !== expected) {
-    return console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+module.exports = head;
